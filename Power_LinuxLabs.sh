e7f8657a1e1a7dd81d0deef115c98eda77d64dc2
RaiLincolnSS <rai.lincoln.silva@gmail.com>
2023-06-07 10:52:17 -0300

    Atualizando a branch

---
+++ Power_LinuxLabs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Variaveis
+BASE_NAME=Linux
+
+#N sera referente as maquinas que serao ligadas, para ligar todas, alterar valor para 10
+#N=4
+
+
+#loop para ligar as maquinas
+for i in {1..4}; do
+  NAME=$BASE_NAME$i
+  multipass start $NAME
+
+done
